a24c8ac1c35882dfae18fb20cb5d38fb10c382a1
aniket singh <aniket.singh@iot83.com>
2024-04-30 08:30:35 +0530

    feat: connected successfully

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,11 +2,12 @@
 //  * Controllers
 //  *-----------------------------------------
 
-// ? In an Express.js application, a "controller" refers to a part of your code that
-// is responsible for handling the application's logic. Controllers are typically
-// used to process incoming requests, interact with models (data sources), and
-// send responses back to clients. They help organize your application by separating
-//  concerns and following the MVC (model view controller) design pattern.
+/* -- In an Express.js application, a "controller" refers to a part of your code that
+      is responsible for handling the application's logic. Controllers are typically
+      used to process incoming requests, interact with models (data sources), and
+      send responses back to clients. They help organize your application by separating
+      concerns and following the MVC (model view controller) design pattern.
+--*/
 
 const User = require("../models/user-model");
 const brcrypt = require("bcryptjs");
